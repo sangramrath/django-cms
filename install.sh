c6ff98c2221c9f26abc63123a5d2689993452e61
sangramrath <sangram.rath@gmail.com>
2023-01-30 14:28:57 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 # This script install Docker / Docker Compose and runs Django CMS
 #!/bin/sh
 apt update -y
-apt install -y docker.io docker-compose
+apt-get install ca-certificates curl gnupg lsb-release
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update -y
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose
+groupadd docker
+usermod -aG docker $USER
 # Clone django cms quickstart
+cd
 git clone https://github.com/django-cms/django-cms-quickstart.git
 cd django-cms-quickstart
 docker-compose build web
