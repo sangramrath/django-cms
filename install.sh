310db74845ca707550b61dbaa7ecc098b254d348
sangramrath <sangram.rath@gmail.com>
2023-01-30 14:58:24 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update -y
-apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose -y
 groupadd docker
 usermod -aG docker $USER
 # Clone django cms quickstart
