7eda26d9280ec780847f0675be572b566e3f47a4
sangramrath <sangram.rath@gmail.com>
2023-02-17 18:03:48 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,12 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update -y
 apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose -y
+cd ~
 git clone https://github.com/django-cms/django-cms-quickstart.git
 cd django-cms-quickstart
+echo 'DJANGO_SUPERUSER_PASSWORD=admin' >> .env-local
 docker-compose build web
 docker-compose up -d database_default
 docker-compose run web python manage.py migrate
-docker-compose run web python manage.py createsuperuser --noinput --username sudjango --email sudjango@local.net
+docker-compose run web python manage.py createsuperuser --noinput --username admin --email admin@djangocms.local
 docker-compose up -d
